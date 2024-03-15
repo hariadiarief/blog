70d96781c03c3143b86443b5e30f4aa274d120c3
hariadiarief <adi.arief77@gmail.com>
2024-03-15 16:14:18 +0700

    fix sawangan code

--- src/app/blog/[...slug]/page.tsx
+++ src/app/blog/[...slug]/page.tsx
@@ -95,16 +95,6 @@ export default async function PostPage({ params }: PostPageProps) {
 
   return (
     <article className='container relative max-w-3xl py-6 lg:py-10'>
-      <Link
-        href='/'
-        className={cn(
-          buttonVariants({ variant: 'ghost' }),
-          'absolute left-[-200px] top-14 hidden xl:inline-flex'
-        )}
-      >
-        <Icons.chevronLeft className='mr-2 h-4 w-4' />
-        See all posts
-      </Link>
       <div>
         {post.date && (
           <time
