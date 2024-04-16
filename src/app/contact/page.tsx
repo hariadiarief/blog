be8f9f1205e14dc6c807ade74f609389edc1bbb4
hariadiarief <adi.arief77@gmail.com>
2024-04-16 16:05:45 +0700

    update contact layout

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,62 +2,49 @@ import { Icons } from '@/components/icons'
 
 export default function contact() {
   return (
-    <div className='container mt-16 max-w-screen-md mx-auto'>
+    <div className='container  flex max-w-screen-md  grow flex-col items-center justify-center'>
       <h3 className='mb-8 text-2xl font-bold'>Contact Me</h3>
 
-      <p className='mb-4'>
-        If you have any question or want to collaborate you can react me out at:
+      <p className='mb-4 text-center'>
+        If you have any question or want to collaborate, you can react me out
+        at:
       </p>
 
-      <div className='align-center flex'>
-        <Icons.mail className='mb-2' />
-        <span className='mx-2'>:</span>
+      <div className='flex flex-wrap justify-center gap-8'>
         <a target='_blank' rel='nofollow' href='mailto:adi.arief77@gmail.com'>
-          adi.arief77@gmail.com
+          <Icons.mail className='mb-2' />
         </a>
-      </div>
-      <div className='align-center flex'>
-        <Icons.twitter className='mb-2' />
-        <span className='mx-2'>:</span>
+
         <a
           target='_blank'
           rel='nofollow'
           href='https://twitter.com/hariadiarief'
         >
-          @hariadiarief
+          <Icons.twitter className='mb-2' />
         </a>
-      </div>
-      <div className='align-center flex'>
-        <Icons.instagram className='mb-2' />
-        <span className='mx-2'>:</span>
+
         <a
           target='_blank'
           rel='nofollow'
           href='https://instagram.com/hariadiarief_'
         >
-          @hariadiarief_
+          <Icons.instagram className='mb-2' />
         </a>
-      </div>
-      <div className='align-center flex'>
-        <Icons.gitHub className='mb-2' />
-        <span className='mx-2'>:</span>
+
         <a
           target='_blank'
           rel='nofollow'
           href='https://github.com/hariadiarief'
         >
-          @hariadiarief
+          <Icons.gitHub className='mb-2' />
         </a>
-      </div>
-      <div className='align-center flex'>
-        <Icons.linkedin className='mb-2' />
-        <span className='mx-2'>:</span>
+
         <a
           target='_blank'
           rel='nofollow'
           href='https://www.linkedin.com/in/hariadiarief/'
         >
-          @hariadiarief
+          <Icons.linkedin className='mb-2' />
         </a>
       </div>
     </div>
